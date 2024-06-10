defb1d3a5d88c022a11b91022c19311d9e609091
willrayner13 <willrayner13@yahoo.co.uk>
2024-06-10 16:11:34 +0100

    Updated Add profile with database

--- routes/addperson-route.js
+++ routes/addperson-route.js
@@ -2,10 +2,25 @@
 
 const express = require('express');
 const router = express.Router();
+const db = require('../db/database');
 
 // Route to render the addperson page
 router.get('/', (req, res) => {
     res.render('addperson'); // Assuming the addperson.ejs file is in the views folder
 });
+
+router.post('/', (req, res) => {
+    const { firstname, surname, dateOfBirth, sex, pin } = req.body;
+    const householdId = req.session.householdId; // Get householdId from session
+
+    db.addProfile(firstname, surname, dateOfBirth, sex, pin, householdId, (err) => { // Pass householdId to addProfile
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Internal Server Error');
+        }
+        res.redirect('/chooseaccount'); // Redirect to chooseaccount page after adding profile
+    });
+});
+
 
 module.exports = router;
