5fd38cab3b164f802da25c4f54003f0f63af4dee
willrayner13 <willrayner13@yahoo.co.uk>
2024-06-10 15:48:05 +0100

    Add profile update

--- server.js
+++ server.js
@@ -1,36 +1,44 @@
+// app.js
+
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const app = express();
 
+// Set view engine and views directory
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
 
+// Middleware
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(session({
     secret: 'mySecretKey123',
     resave: false,
     saveUninitialized: false
 }));
 
+// Import routes
 const loginRouter = require('./routes/login-route');
-const chooseaccountRouter = require('./routes/chooseaccount-route'); 
+const chooseaccountRouter = require('./routes/chooseaccount-route');
 const dashboardRouter = require('./routes/dashboard-route');
-const prescriptionRouter = require('./routes/prescription-route'); // Import the prescriptions route
+const prescriptionRouter = require('./routes/prescription-route');
+const addpersonRouter = require('./routes/addperson-route'); // Import the addperson route
 
+// Use routes
 app.use(loginRouter);
 app.use('/dashboard', dashboardRouter);
 app.use('/chooseaccount', chooseaccountRouter);
-app.use('/prescription', prescriptionRouter); // Mount the prescriptions route
+app.use('/prescription', prescriptionRouter);
+app.use('/chooseaccount/add-profile', addpersonRouter); // Mount the addperson route
 
+// Redirect root to login
 app.get('/', (req, res) => {
     res.redirect('/login');
 });
 
+// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
